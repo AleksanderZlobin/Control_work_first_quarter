72a38a8c02f457af38d4b7114f22d58c75df0b49
AleksanderZlobin <lancer111187@mail.ru>
2023-07-02 12:50:34 +0700

    add program code alternative solution

--- Program.cs
+++ Program.cs
@@ -3,21 +3,49 @@
 // либо задать на старте выполнения алгоритма. При решении не рекомендуется пользоваться коллекциями, 
 // лучше обойтись исключительно массивами.
 
+//// # Main
+// Console.Clear();
+// string[] array = new string[] { "Sun", "Monday", "Tu", "Wednesday", "Thu", "F", "Saturday" };
+// string[] array_new = new string[array.Length];
+// int count = 0;
+// for (int i = 0; i < array.Length; i++)
+// {
+//     if (array[i].Length <= 3)
+//     {
+//         array_new[count] = array[i];
+//         count++;
+//     }
+// }
+// Console.WriteLine($"Конечный массив: [{string.Join(" ", array_new)}]");
 
-Console.Clear();
-string[] array = new string[] { "Sun", "Monday", "Tu", "Wednesday", "Thu", "F", "Saturday" };
-string[] array_new = new string[array.Length];
-int count = 0;
-for (int i = 0; i < array.Length; i++)
-{
-    if (array[i].Length <= 3)
-    {
-        array_new[count] = array[i];
-        count++;
-    }
-}
-// Console.WriteLine($"Начальный массив: [{string.Join(" ", array)}]");
-Console.WriteLine($"Конечный массив: [{string.Join(" ", array_new)}]");
+
+// // # Alternative
+// Console.Clear();
+// string[] array = new string[] { "Sun", "Monday", "Tu", "Wednesday", "Thu", "F", "Saturday" };
+// int n = 0;
+// for (int i = 0; i < array.Length; i++)
+// {
+//     if (array[i].Length <= 3)
+//     {
+//         n++;
+//     }
+// }
+
+// string[] array_new = new string[n];
+
+// int count = 0;
+// for (int i = 0; i < array.Length; i++)
+// {
+//     if (array[i].Length <= 3)
+//     {
+//         array_new[count] = array[i];
+//         count++;
+//     }
+// }
+// Console.WriteLine($"Конечный массив: [{string.Join(", ", array_new)}]");
+
+
+
 
 
 
